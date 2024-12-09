3d8152e143d209798c880eab3e7fa94fee96c300
otariidae <otariidae@users.noreply.github.com>
2024-12-09 19:31:58 +0900

    biome lint --write --unsafe

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ export async function run(): Promise<void> {
 
     // Log the current timestamp, wait, then log the new timestamp
     core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
+    await wait(Number.parseInt(ms, 10))
     core.debug(new Date().toTimeString())
 
     // Set outputs for other workflow steps to use
